c3633a50059aa817755e7b9accf0a05e5989ae16
sreparthiban <sreparthibanr@gmail.com>
2017-04-02 20:55:40 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,14 @@
 function click(e){
 	alert("hi");
-	chrome.tabs.query({},function(tabs){     
-    console.log("background.js : click()");
-    tabs.forEach(function(tab){
-    console.log(tab.url);
 	
-    });
+chrome.tabs.query({}, function(tabs) {
+    var i;
+    for (i=0; i<tabs.length; i++) {
+    	var d = tabs[i].url;
+    	alert(d);
+    }
  });
+
 }
 chrome.browserAction.onClicked.addListener(click);
+
