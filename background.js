a602dddde7e45607c96de1cbd7ff08633d3d0e6c
sreparthiban <sreparthibanr@gmail.com>
2017-04-12 00:51:17 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,39 @@
 function click(e){
-	alert("hi");
-	
+		
+	  	
 chrome.tabs.query({}, function(tabs) {
     var i;
+	var d = [tabs.length];
     for (i=0; i<tabs.length; i++) {
-    	var d = tabs[i].url;
-    	alert(d);
-    }
+         d[i] = tabs[i].url;
+	}
+
+	
+	var str = prompt("Please enter the Folder name");
+		
+	chrome.bookmarks.create({
+		title: str,
+		parentId: "1"
+	}, 
+	
+	function (newEntry) {
+	    add(newEntry);
+	   
+	});
+
+	 function add(entry){
+		
+		for(i=0; i<d.length; i++){
+		chrome.bookmarks.create({
+			'title': tabs[i].title,
+			'url': d[i],
+			'parentId': entry.id
+		});
+		console.log('New Entries Added');
+		}
+	 }
+	
  });
-
 }
-chrome.browserAction.onClicked.addListener(click);
 
+chrome.browserAction.onClicked.addListener(click);
